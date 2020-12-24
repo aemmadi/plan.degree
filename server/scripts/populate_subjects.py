8f48ce945a5b56b604247a7933518fc00dd682c5
Anirudh Emmadi <kanna6501@gmail.com>
2020-12-24 17:35:56 -0600

    chore: insert course numbers to db (#5)

--- server/scripts/populate_subjects.py
+++ server/scripts/populate_subjects.py
@@ -1,5 +1,20 @@
-from ..db import client
+from db import client
 
 file = open("archive/all_course_names.txt", "r")
 
-print(file.readline())
+course_list = client["catalog_data"]["course_list"]
+data = []
+
+while True:
+    subject = file.readline()
+    if subject == "":
+        break
+    course = subject[:-1]
+    course = course.split(" ")
+    course = {'subj': course[0], 'num': course[1]}
+
+    tmp = {'course': course}
+    data.append(tmp)
+
+# PUSH TO DB
+# course_list.insert_many(data)
