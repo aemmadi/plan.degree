b4d162ca807221898a0642ea6737e3c05cc1fe5c
Anirudh Emmadi <kanna6501@gmail.com>
2021-01-31 21:03:24 -0600

    chore: implemented basic design (#8)

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,91 @@
 import React from 'react'
-import { Provider } from 'rebass'
+import {Button, Card, Container, Header, Icon, Input, Menu} from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
 
-import Login from './Components/Login/Login'
+import Logout from './Components/Core/Logout'
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      activeItem: 'home',
+      loggedInUser: 'Bob Smith'
+    }
+  }
 
-const App = props => (
-  <Provider>
-    <Login/>
-  </Provider>
-)
+  componentDidMount() {
 
-export default App;
+  }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+  const { activeItem } = this.state
+    return (
+      <div id="app">
+        <Menu style={{height: '4em'}}>
+          <Menu.Item
+            name='dashboard'
+            active={activeItem === 'dashboard'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='planner'
+            active={activeItem === 'planner'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Menu 
+            content="Plan.Degree" 
+            position='right'
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              fontFamily: 'sketch-3d',
+              fontSize: '2em',
+              fontWeight: 'light',
+              marginRight: '0.9em'
+          }}/>
+          <Menu.Menu position='right'>
+            <Menu.Item
+              name='settings'
+              active={activeItem === 'settings'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name='logout'
+              active={activeItem === 'logout'}
+              onClick={this.handleItemClick}
+              style={{
+                color: '#b23b3b'
+              }}
+            />
+          </Menu.Menu>
+        </Menu>
+        <Container>
+          <Header 
+            size='huge'
+            content="Welcome Bob!"
+            style={{
+              marginTop: '2em',
+              textAlign: 'center'
+            }}
+          />
+          <Header 
+            as="h2"
+            content="Degree Plans"
+            style={{
+              textAlign: 'center'
+            }}
+          />
+          <center style={{marginBottom: '1em'}}>
+            <Button color='blue'>New Degree Plan</Button>{' '}
+            <Button color='red'>Remove Plan</Button>
+          </center>
+          <Card.Group>
+            <Card fluid as={Link} to="/demo" color='orange' header="My First Degree Plan" meta="Automatically generated degree plan." />
+          </Card.Group>
+        </Container>
+      </div>
+    )
+  }
+}
