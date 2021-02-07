19c51f7bb4fa084c97e8438d957bbb1d6020f5f1
Anirudh Emmadi <kanna6501@gmail.com>
2021-02-06 23:46:54 -0600

    chore: added handlers for click events

--- client/src/Components/Planner/components/course.jsx
+++ client/src/Components/Planner/components/course.jsx
@@ -2,32 +2,58 @@ import React from 'react';
 import styled from 'styled-components';
 import {Draggable} from 'react-beautiful-dnd';
 
-const Container = styled.div`
-  border: 2px solid lightgrey;
-  border-radius: 10%;
-  padding: 8px;
-  margin-right: 8px;
-  background-color: ${props => props.isDragging ? 'lightGreen' : 'white'};
-  width: 100px;
-  height: 50px;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
 export default class Course extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      clicked: false,
+    }
+  }
+
+  handleCourseClick = (e) => {
+    const course = e.target.innerText
+    this.setState({
+      spotlight: course,
+      clicked: false
+    })
+  }
+
+  handleCourseTrigger = () => {
+    const click = this.state.clicked
+    this.setState({
+      clicked: !click
+    })
+  }
+  
   render() {
+    const Container = styled.div`
+    border: 1px solid lightgrey;
+    border-radius: 10%;
+    padding: 8px;
+    margin-right: 8px;
+    background-color: ${props => props.isDragging || this.state.clicked ? 'lightGrey' : 'white'};
+    width: 100px;
+    height: 50px;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    `;
     return (
       <Draggable draggableId={this.props.course.id} index={this.props.index}>
         {(provided, snapshot) => (
           <Container
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-            ref={provided.innerRef}
-            isDragging={snapshot.isDragging}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          isDragging={snapshot.isDragging}
+          content={this.props.course.content}
+          onClick={this.handleCourseClick}
+          onMouseDown={this.handleCourseTrigger}
+          onMouseUp={this.handleCourseTrigger}
           >
-            {this.props.course.content}
+            {console.log(this.state)}
+            <strong>{this.props.course.content}</strong>
           </Container>
         )}
       </Draggable>
