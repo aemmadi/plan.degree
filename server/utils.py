7d213eccc39a3d4576145727e03070a0faf79822
Anirudh Emmadi <kanna6501@gmail.com>
2021-11-14 10:58:40 -0600

    feat: retrieve user data endpoints

--- server/utils.py
+++ server/utils.py
@@ -3,6 +3,7 @@ from bson import json_util
 import hashlib
 import uuid
 
+
 def format_tag(tag):
     return tag.upper()
 
@@ -15,10 +16,18 @@ def set_headers(res):
     res.headers["Content-Type"] = "application/json"
     return res
 
+
 def generate_random_token():
     id = uuid.uuid4().hex.encode('utf-8')
     return hashlib.sha256(id).hexdigest()
 
+
 def generate_hash(msg):
-    msg = msg.encode('utf-8')
     return hashlib.sha256(msg).hexdigest()
+
+
+def verify_signature(signature, email, key):
+    # check if signature hash is valid, prevents basic db attacks
+    if generate_hash(f'{email}{key}') is not signature:
+        return False
+    return True
