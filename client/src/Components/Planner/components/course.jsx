61c14660f5bacd06884d66bb30ad3edcdd322381
Anirudh Emmadi <kanna6501@gmail.com>
2021-11-14 10:57:24 -0600

    feat: display course info

--- client/src/Components/Planner/components/course.jsx
+++ client/src/Components/Planner/components/course.jsx
@@ -3,6 +3,23 @@ import styled from 'styled-components';
 import {Draggable} from 'react-beautiful-dnd';
 import {Modal} from 'semantic-ui-react'
 export default class Course extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      course: {},
+    }
+
+    this.handleCourseClick = this.handleCourseClick.bind(this);
+  }
+
+  handleCourseClick() {
+    fetch(`${process.env.REACT_APP_API_URL}/course/search/${this.props.course.content}`).then(res => res.json()).then(data => {
+      this.setState({
+        course: data.results[0]
+      })
+    })
+  }
+
   render() {
     const Container = styled.div`
     border: 1px solid lightgrey;
@@ -34,7 +51,18 @@ export default class Course extends React.Component {
               </Container>
             }
           >
-            <Modal.Header>{this.props.course.content}</Modal.Header>
+            <Modal.Header>{this.props.course.content} - {this.state.course.title}</Modal.Header>
+            <Modal.Content>
+              <h3><strong>
+                Description</strong></h3>
+                {' '}
+                {this.state.course.desc}
+
+              <h3><strong>Pre-Reqs/Co-Reqs</strong></h3>
+              {' '}
+              {this.state.course.req_text}
+            </Modal.Content>
+
           </Modal>
         )}
       </Draggable>
